d391d19f8c69eadf5fe8c6b5027249ca4c5284e9
Rama <Rama>
2019-12-23 09:45:45 +1100

    until today

--- JavaTPoint/src/Simple.java
+++ JavaTPoint/src/Simple.java
@@ -2,6 +2,6 @@ public class Simple {
 
     public static void main(String args[])
     {
-        system.out.println("Welcome to Simple Class");
+        System.out.println("Welcome to Simple Class");
     }
 }
