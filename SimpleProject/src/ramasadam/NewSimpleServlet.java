6c8a1e1c9b91088bd8e4a7daf49fe2a8167834e4
Rama <sadamramanayudu@gmail.com>
2020-04-14 06:14:13 +1000

    claanup

--- SimpleProject/src/ramasadam/NewSimpleServlet.java
+++ SimpleProject/src/ramasadam/NewSimpleServlet.java
@@ -2,9 +2,10 @@ package ramasadam;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(name = "newSimpleServlet", urlPatterns = "/first")
+//@WebServlet(name = "newSimpleServlet", urlPatterns = "/first")
 public class NewSimpleServlet extends HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request,
                           javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException,
