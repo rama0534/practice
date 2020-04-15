ae4227283d74f87b680702aee49ea0b05eefe96f
Rama <sadamramanayudu@gmail.com>
2020-04-16 05:28:37 +1000

    Tut_7JSPs and Servlets Tutorial 06 - Passing More Parameters

--- SimpleProject/src/ramasadam/NewSimpleServlet.java
+++ SimpleProject/src/ramasadam/NewSimpleServlet.java
@@ -12,8 +12,18 @@ public class NewSimpleServlet extends HttpServlet {
             IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String UserName = request.getParameter("UserName");
-        out.println("Hello from doPost method " + UserName);
+        String userName = request.getParameter("userName");
+        String fullName = request.getParameter("fullName");
+        out.println("Hello doPost " + userName+ "we know your full name   "+ userName +"  "+ fullName  );
+        String prof = request.getParameter("prof");
+        out.println("your are a  "+prof);
+//        String location = request.getParameter("location");
+        String[] location = request.getParameterValues("location");
+        out.println("you are at  "+location.length+" places  ");
+        for (int i=0 ; i<location.length; i++){
+            out.println(location[i]);
+        }
+
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request,
@@ -21,7 +31,8 @@ public class NewSimpleServlet extends HttpServlet {
             IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String UserName = request.getParameter("UserName");
-        out.println("Hello doGet " + UserName);
+        String userName = request.getParameter("userName");
+        String fullName = request.getParameter("fullName");
+        out.println("Hello doGet " + userName+ "we know your full name"+ fullName);
     }
 }
