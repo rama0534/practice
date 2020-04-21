c8e66cbdcc74bd54bc783a4c2b5bae8b079b5ba8
Rama <sadamramanayudu@gmail.com>
2020-04-21 11:17:08 +1000

    JSPs and Servlets Tutorial 09 Part 2- Understanding init, service and ServletConfig

--- SimpleProject/src/ramasadam/NewSimpleServlet.java
+++ SimpleProject/src/ramasadam/NewSimpleServlet.java
@@ -5,11 +5,13 @@ import org.omg.IOP.ServiceContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 
-//@WebServlet(name = "newSimpleServlet", urlPatterns = "/NewSimpleServletPaths")
+@WebServlet(name = "newSimpleServlet", urlPatterns = "/NewSimpleServletPaths")
+
 public class NewSimpleServlet extends HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request,
                           javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException,
@@ -46,6 +48,7 @@ public class NewSimpleServlet extends HttpServlet {
         out.println("Request parameter has username as "+ userName);
         out.println("Session parameter has user name as "+ (String) session.getAttribute("savedUserName"));
         out.println("Context parameter has user name as "+ (String) context.getAttribute("savedUserName"));
+        this.getServletConfig().getInitParameter("defaultUser");
 
 //        String fullName = request.getParameter("fullName");
 //        out.println("Hello doGet " + userName+ "we know your full name"+ fullName);
