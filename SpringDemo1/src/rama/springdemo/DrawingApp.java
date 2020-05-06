ffbb0f29a14a4d8a97c9bdd7450a3141938ebdbf
Rama <sadamramanayudu@gmail.com>
2020-05-07 03:03:10 +1000

    Spring Tutorial 05 - ApplicationContext and Property Initialization

--- SpringDemo1/src/rama/springdemo/DrawingApp.java
+++ SpringDemo1/src/rama/springdemo/DrawingApp.java
@@ -3,6 +3,8 @@ package rama.springdemo;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
@@ -10,9 +12,10 @@ public class DrawingApp {
     public static void main(String args[]){
 
 //        Triangle triangle = new Triangle();
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle = (Triangle) factory.getBean("triangle");
-
+//        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//        Triangle triangle = (Triangle) factory.getBean("triangle");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
 
     }
--- SpringDemo1/src/rama/springdemo/Triangle.java
+++ SpringDemo1/src/rama/springdemo/Triangle.java
@@ -3,8 +3,20 @@ package rama.springdemo;
 
 public class Triangle {
 
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+
     public void draw(){
-        System.out.println("Triangle drawn!");
+        System.out.println(getType()+    "Triangle drawn!");
     }
 
 
