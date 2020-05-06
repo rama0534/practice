ad869643b932dfc63d65ff6374eb698ea5d349f6
Rama <sadamramanayudu@gmail.com>
2020-05-07 03:47:47 +1000

    Spring Tutorial 06 - Using Constructor Injection

--- SpringDemo1/src/rama/springdemo/Triangle.java
+++ SpringDemo1/src/rama/springdemo/Triangle.java
@@ -4,19 +4,36 @@ package rama.springdemo;
 public class Triangle {
 
     private String type;
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Triangle(String type){
+        this.type = type;
+    }
+
+    public Triangle(int height){
+        this.height = height;
+    }
+    public Triangle(String type, int height){
+        this.type = type;
+        this.height = height;
+    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 
 
 
     public void draw(){
-        System.out.println(getType()+    "Triangle drawn!");
+        System.out.println(getType()  +    "Triangle drawn!"+    getHeight());
     }
 
 
