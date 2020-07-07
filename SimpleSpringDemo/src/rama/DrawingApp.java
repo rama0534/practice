5e96d4e460ed61f5d8cd6d58812fd26cee5be692
Rama <sadamramanayudu@gmail.com>
2020-07-08 02:25:49 +1000

    Spring Tutorial 13 - Bean Definition Inheritance

--- SimpleSpringDemo/src/rama/DrawingApp.java
+++ SimpleSpringDemo/src/rama/DrawingApp.java
@@ -14,7 +14,7 @@ public class DrawingApp {
 
 //        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Triangle triangle =(Triangle) context.getBean("triangle");
+        Triangle triangle =(Triangle) context.getBean("triangle2");
         triangle.draw();
 
     }
