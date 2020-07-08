4c6f03456dee853b38126d5d4f76df180371ccd5
Rama <sadamramanayudu@gmail.com>
2020-07-09 05:49:38 +1000

    Spring Tutorial 20 - Some JSR-250 Annotations

--- SimpleSpringDemo/src/rama/Circle.java
+++ SimpleSpringDemo/src/rama/Circle.java
@@ -2,6 +2,10 @@ package rama;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 //import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape {
@@ -22,10 +26,21 @@ public class Circle implements Shape {
     }
 
 
-    @Autowired
-    @Qualifier("circleRelated")
+    @Resource
     public void setCenter(Point center) {
         this.center = center;
+    }
+
+    @PostConstruct
+    public void initializeCircle(){
+
+        System.out.println("Init of Circle");
+    }
+
+    @PreDestroy
+    public void destroyCircle(){
+
+        System.out.println("Destroy of Circle");
     }
 
 
