56b3cc89a83b8928ca31147e2f9816eb3718decb
Rama <sadamramanayudu@gmail.com>
2020-07-13 23:31:40 +1000

    Spring Tutorial 28 - Pointcuts and Wildcard Expressions

--- SpringAop/src/aspect/LoggingAspect.java
+++ SpringAop/src/aspect/LoggingAspect.java
@@ -2,12 +2,25 @@ package aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution( public String getName())")
+    @Before("allGetters())")
     public void LoggingAdvice(){
         System.out.println("Advice run. Get method called");
     }
+
+    @Before("allGetters())")
+    public void secondAdvices(){
+        System.out.println("Second Advice Executed");
+    }
+
+    @Pointcut("execution(* get*())")
+    public void allGetters(){
+        
+    }
+
+
 }
