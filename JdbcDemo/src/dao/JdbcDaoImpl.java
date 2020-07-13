5681a0293f26c4d7e8164e92265a4aeaf23aa619
Rama <sadamramanayudu@gmail.com>
2020-07-14 04:24:20 +1000

    Spring Tutorial 37 - Using JDBC without Spring

--- JdbcDemo/src/dao/JdbcDaoImpl.java
+++ JdbcDemo/src/dao/JdbcDaoImpl.java
@@ -1,4 +1,46 @@
 package dao;
 
+import model.Circle;
+import org.apache.derby.client.am.SqlException;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JdbcDaoImpl {
-}
+
+
+    public Circle getCircle(int circlId) throws ClassNotFoundException, SQLException, IllegalAccessException,
+            InstantiationException {
+
+
+        Connection conn = null;
+        String  driver = "org.apache.derby.jdbc.ClientDriver";
+        Class.forName(driver).newInstance();
+        conn = DriverManager.getConnection(("jdbc:derby://localhost:1527/db"));
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM circle where id=?");
+        ps.setInt(1,circlId);
+
+        Circle circle = null;
+        ResultSet rs= ps.executeQuery();
+        if(rs.next()){
+            circle = new Circle(circlId, rs.getString("name"));
+        }
+
+        rs.close();
+        ps.close();
+        return circle;
+
+
+
+
+        }
+
+
+    }
+
+
+
--- JdbcDemo/src/rama/JdbcDemo.java
+++ JdbcDemo/src/rama/JdbcDemo.java
@@ -1,7 +1,18 @@
 package rama;
 
+import dao.JdbcDaoImpl;
+import model.Circle;
+
+import javax.sql.rowset.JdbcRowSet;
+import java.sql.SQLException;
+
 public class JdbcDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException,
+            IllegalAccessException {
+
+
+        Circle circle = new JdbcDaoImpl().getCircle(1);
+        System.out.println(circle.getName());
 
     }
 }
