dffa06f919ac222a0e6bdd2b207e3d96774d8f25
Rama <sadamramanayudu@gmail.com>
2020-07-17 04:10:14 +1000

    Spring MVC Tutorials 09 - @PathVariable annotation in detail (A URI-Template concept )

--- SpringMVC/src/com/practice/hello/HelloController.java
+++ SpringMVC/src/com/practice/hello/HelloController.java
@@ -1,12 +1,14 @@
 package com.practice.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 
 //public class HelloController extends AbstractController {
@@ -20,27 +22,30 @@ import javax.servlet.http.HttpServletResponse;
 //    }
 //}
 @Controller
-@RequestMapping("/greet")
+//@RequestMapping("/greet")
 public class HelloController {
 
-    @RequestMapping("/welcome")
-    public ModelAndView helloWorld() {
+    @RequestMapping("/welcome/{countryName}/{userName}")
+    public ModelAndView helloWorld(@PathVariable Map<String,String> pathVars) {
+
+        String name = pathVars.get("userName");
+        String country = pathVars.get("countryName");
 
         ModelAndView model = new ModelAndView("/HelloPage");
         //    model.addObject("HelloWorld");
-        model.addObject("msg", "Hello World");
+        model.addObject("msg", "Hello  "+ name + "  you are from " +country);
 
         return model;
     }
 
-    @RequestMapping("/hi")
-    public ModelAndView hiWorld() {
-
-        ModelAndView model = new ModelAndView("/HelloPage");
-
-        model.addObject("msg", "Hi World");
-
-        return model;
-    }
+//    @RequestMapping("/hi")
+//    public ModelAndView hiWorld() {
+//
+//        ModelAndView model = new ModelAndView("/HelloPage");
+//
+//        model.addObject("msg", "Hi World");
+//
+//        return model;
+//    }
 
 }
