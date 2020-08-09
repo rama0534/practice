fd43179560665f3340b26fb884531c515db55124
Rama <sadamramanayudu@gmail.com>
2020-08-10 01:46:30 +1000

    Maven Tutorial 02,03 &  04 - Adding a Dependency

--- myapp/MavenTestApp/src/main/java/rama/App.java
+++ myapp/MavenTestApp/src/main/java/rama/App.java
@@ -1,5 +1,5 @@
 package rama;
-
+import org.slf4j.*;
 /**
  * Hello world!
  *
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World!");
     }
 }
