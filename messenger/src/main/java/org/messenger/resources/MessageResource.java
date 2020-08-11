4345ed57a6940068fd6cabeb8fa90e55dda8f512
Rama <sadamramanayudu@gmail.com>
2020-08-12 03:39:58 +1000

    REST Web Services 19 - Implementing POST Method

--- messenger/src/main/java/org/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.messenger.resources;
 import org.messenger.model.Message;
 import org.messenger.service.MessageService;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +24,13 @@ public class MessageResource {
     public List<Message> getMessages() {
 
         return messageService.getAllMessages();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Message addMessage(Message message){
+        return messageService.addMessage(message);
     }
 
     @GET
