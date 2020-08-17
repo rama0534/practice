544a66a8a8eb3caf258ebade623492be9b23d5ee
Rama <sadamramanayudu@gmail.com>
2020-08-18 03:26:54 +1000

    Spring Boot Quick Start 10 - Spring Boot startup steps

---
+++ course-api/src/test/java/com/rama/springbootquickstart/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rama.springbootquickstart.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
