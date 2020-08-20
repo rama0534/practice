2205e32c2b21c3eeba7a04dba892115c18348041
Rama <sadamramanayudu@gmail.com>
2020-08-20 23:33:37 +1000

    Spring Boot Quick Start 27 - Adding Spring Data JPA

---
+++ course-api-data/src/main/java/com/example/rama/courseapidata/topic/TopicService.java
@@ -0,0 +1,44 @@
+package com.example.rama.courseapidata.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("string", "spring framework", "spring framework description"),
+            new Topic("Java", "  Core Java", "Core Java description"),
+            new Topic("JavaScript", "JavaScript", "JavaScript description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t ->t.getId().equals(id));
+    }
+}
