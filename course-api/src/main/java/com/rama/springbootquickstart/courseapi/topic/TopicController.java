52a1bbab1edfc9c95889a3dac243c273ce72ce60
Rama <sadamramanayudu@gmail.com>
2020-08-20 16:45:29 +1000

    Spring Boot Quick Start 18 - Getting a single resource

--- course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicController.java
+++ course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicController.java
@@ -1,6 +1,7 @@
 package com.rama.springbootquickstart.courseapi.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,9 @@ public class TopicController {
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
+    }
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
     }
 }
--- course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicService.java
+++ course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicService.java
@@ -16,4 +16,8 @@ public class TopicService {
     public List<Topic> getAllTopics(){
         return topics;
     }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
 }
