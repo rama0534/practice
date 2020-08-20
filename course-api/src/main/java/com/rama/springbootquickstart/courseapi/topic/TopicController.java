458ded3d8f940c71f1e0d2d7a0469b3f0deedf40
Rama <sadamramanayudu@gmail.com>
2020-08-20 22:48:42 +1000

    Spring Boot Quick Start 20 - Implementing Update and Delete

--- course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicController.java
+++ course-api/src/main/java/com/rama/springbootquickstart/courseapi/topic/TopicController.java
@@ -15,17 +15,29 @@ public class TopicController {
 
     @Autowired
     private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
+
     @RequestMapping("/topics/{id}")
-    public Topic getTopic(@PathVariable String id){
+    public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
+
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
-    public void addTopic(@RequestBody Topic topic)
-    {
+    public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+          topicService.deleteTopic(id);
     }
 }
