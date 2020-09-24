5b40220f8850377361d4635ac48e29e8cecba13a
Rama <sadamramanayudu@gmail.com>
2020-09-24 11:57:12 +1000

    Java Script Basics

--- JavaTPoint/src/Simple.java
+++ JavaTPoint/src/Simple.java
@@ -1,7 +1,19 @@
+
+
+// Creating Simple class
 public class Simple {
 
-    public static void main(String args[])
-    {
-        System.out.println("Welcome to Simple Classes");
+    //defining fields
+    int id = 10;//data members
+    String name;
+
+    //Creating main method in Simple class
+    public static void main(String args[]) {
+        //Creating an objector instance
+
+        Simple simple = new Simple();
+        //Printing Values of the Object
+        System.out.println(simple.id);
+        System.out.println(simple.name);
     }
 }
