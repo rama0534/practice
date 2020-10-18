fe1b30f3f45f7d02888617e63b8aed72e54d3497
Rama <sadamramanayudu@gmail.com>
2020-10-19 04:25:53 +1100

    Conditional Rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,34 +3,24 @@ class Counter extends Component {
     state = {  
         count: 0,
         // imageUrl: "https://picsum.photos/200"
-        tags: ["tag1", "tag2", "tag3"]
+        tags: []
 
     };
     // styles = {
     //     fontSize: 10,
     //     fontWeight: "bold"
     // };
+    renderTags() {
+        if(this.state.tags.length === 0) return <p> There is no tags!</p>
+        return <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>
+    } 
     render() { 
         
         return (
             <div>
-                {/* <img src={this.state.imageUrl} alt=''/> */}
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                 <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>
+                {this.renderTags()}
             </div>
             );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
     }
 }
 
