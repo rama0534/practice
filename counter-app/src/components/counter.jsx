9dcf455b1356381ace43fdcf49ebd556e2bf9380
Rama <sadamramanayudu@gmail.com>
2020-10-19 04:04:15 +1100

    React Tutorial for Beginers upto Rendering Lists

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {  
+        count: 0,
+        // imageUrl: "https://picsum.photos/200"
+        tags: ["tag1", "tag2", "tag3"]
+
+    };
+    // styles = {
+    //     fontSize: 10,
+    //     fontWeight: "bold"
+    // };
+    render() { 
+        
+        return (
+            <div>
+                {/* <img src={this.state.imageUrl} alt=''/> */}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                 <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>
+            </div>
+            );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+
+ 
+export default Counter;
