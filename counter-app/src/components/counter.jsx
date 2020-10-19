d226042a415569bfe3392687721041e3ca8042c1
Rama <sadamramanayudu@gmail.com>
2020-10-19 20:03:23 +1100

    React:Composing components, Passing Data to Componets And Passing Children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {  
-        count: 0,
+        value: this.props.value
          
 
     };
     handleIncrement = product => {
         console.log(product);
-        this.setState({count: this.state.count + 1 });
+        this.setState({value: this.state.value + 1 });
     };
     doHandleIncrement = () => {
         this.handleIncrement({id: 1});
@@ -28,11 +28,11 @@ class Counter extends Component {
     }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
