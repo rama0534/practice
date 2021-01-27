fee57bf63a3b1fcaba02edba3f1f9f28770f03aa
Rama <sadamramanayudu@gmail.com>
2021-01-27 13:23:31 +1100

    Creating and hosting a Full-Stack Site-Backend-Adding comments

--- my-blog/my-blog-backend/src/server.js
+++ my-blog/my-blog-backend/src/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 const articleInfo = {
     'learn-react' : {
         upvotes: 0,
+        comments: [],
     },
     'learn-node' :{
         upvotes: 0,
+        comments: [],
     },
     'my-thoughts-on-resumes' : {
         upvotes: 0,
+        comments: [],
     },
 }
 const app = express();
@@ -16,5 +19,11 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articleInfo[articleName].upvotes += 1;
     res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes`);
+})
+app.post('/api/articles/:name/add-comment',(req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    articleInfo[articleName].comments.push({ username, text });
+    res.status(200).send(articleInfo[articleName]);
 })
 app.listen(8000, () => console.log('Listening on port 8080'));
