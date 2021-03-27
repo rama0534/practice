9ddc6ab596eb17c5de1626bbf7398304b819c1a8
Rama <sadamramanayudu@gmail.com>
2021-03-27 13:23:21 +1100

    React-Basics

--- react-basics/src/components/counter.jsx
+++ react-basics/src/components/counter.jsx
@@ -10,13 +10,20 @@ class Counter extends Component {
         fontSize: 20,
         fontWeight: 'bold'
     };
-     
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There is no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)}</ul>
+    }
+    handleIncrement = () => {
+        // console.log("Increment", this)
+        this.setState({count : this.state.count + 1});
+    }
     render() { 
         return (
         <div>
         <span style = {this.styles} className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-        <button style = {{ fontSize: 20}} type="button" class="btn btn-secondary btn-lg m-2" >Increment</button>
-        <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)}</ul>
+        <button style = {{ fontSize: 20}} type="button" className="btn btn-secondary btn-lg m-2" onClick={this.handleIncrement}>Increment</button>
+        {this.renderTags()}
         </div>);
     }
     getBadgeClasses() {
